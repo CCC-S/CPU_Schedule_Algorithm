3007c8ea955c7d29a3c4656348d429e5df7da89b
CCC-S <0empcjs@gmail.com>
2017-11-22 18:38:16 +0800

    modify the SJF algorithm and update the input.txt

--- src/scheduleAlgorithm/ShortestJobFirst.java
+++ src/scheduleAlgorithm/ShortestJobFirst.java
@@ -4,6 +4,7 @@ import models.BurstUnit;
 import models.Process;
 import models.Schedule;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,22 +16,43 @@ public class ShortestJobFirst extends Schedule {
 
     @Override
     public void scheduleAlgorithm(){
-        Collections.sort(processes, Process.sortByRunningTime);
+        //Collections.sort(processes, Process.sortByRunningTime);
         Collections.sort(processes, Process.sortByArrivalTime);
+
+        List<Process> arrivedProcess = new ArrayList<>();
+        int numOfFinished = 0;
 
         int currentTime = 0;
         int startTime, endTime;
-        for (Process process : processes) {
-            if (currentTime < process.getArrivalTime()) {
-                currentTime = process.getArrivalTime();
-                process.setWaitingTime(0);
-            } else {
-                process.setWaitingTime(currentTime - process.getArrivalTime());
+
+        while (numOfFinished < processes.size()){
+            for (int i = arrivedProcess.size()+numOfFinished; i < processes.size(); i++) {
+                if (processes.get(i).getArrivalTime() <= currentTime) {
+                    arrivedProcess.add(processes.get(i));
+                }
             }
+
+            /**
+             * When those arrived process were all finished, skipping the time of idle and moving to the next process.
+             */
+            if (arrivedProcess.size() == 0){
+                currentTime = processes.get(numOfFinished).getArrivalTime();
+                continue;
+            }
+
+            Collections.sort(arrivedProcess, Process.sortByRunningTime);
+
             startTime = currentTime;
-            currentTime += process.getRunningTime();
+
+            currentTime += arrivedProcess.get(0).getRunningTime();
             endTime = currentTime;
-            burstUnits.add(new BurstUnit(process.getName(), startTime, endTime));
+
+            burstUnits.add(new BurstUnit(arrivedProcess.get(0).getName(), startTime, endTime));
+
+            arrivedProcess.get(0).setWaitingTime(currentTime - arrivedProcess.get(0).getArrivalTime() - arrivedProcess.get(0).getRunningTime());
+            arrivedProcess.remove(arrivedProcess.get(0));
+            numOfFinished += 1;
+
         }
 
     }
